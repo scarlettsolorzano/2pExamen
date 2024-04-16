ecdd83ec67ca9adda6b7d248704dab5d0f3c06db
Scarlett <ssolorzano0@ucol.mx>
2024-04-16 11:22:03 -0600

    examen

---
+++ Examen2P/Examen2P/Data/Inventario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Examen2P.Data
+{
+    public class Inventario
+    {
+        [Required(ErrorMessage = "Debe ser un número entero positivo mayor a 0")]
+        [Range(1, int.MaxValue, ErrorMessage = "El número de inventario debe ser un entero positivo.")]
+        public int NoInventario { get; set; }
+
+        [Required(ErrorMessage = "El nombre no debe estar vacío")]
+        [StringLength(200, ErrorMessage = "Excede el máximo de caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar el departamento")]
+        public string Departamento { get; set; }
+    }
+}
